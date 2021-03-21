bff2d2a02f37090419213d9d6e21149e13582dc6
Emilio Gozo <emil.gozo@gmail.com>
2021-03-22 02:35:31 +0800

    fix: exclude transpiling mapbox-gl

--- craco.config.js
+++ craco.config.js
@@ -2,6 +2,11 @@ const tailwindcss = require('tailwindcss')
 const autoprefixer = require('autoprefixer')
 
 module.exports = {
+  babel: {
+    loaderOptions: {
+      ignore: ['./node_modules/mapbox-gl/dist/mapbox-gl.js'],
+    },
+  },
   style: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
