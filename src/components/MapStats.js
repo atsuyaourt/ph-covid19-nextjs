3444d5762a5bd1a5ff954eb5dd43023f47252c56
Emilio Gozo <emil.gozo@gmail.com>
2021-04-11 21:30:57 +0800

    feat: add total count

--- src/components/MapStats.js
+++ src/components/MapStats.js
@@ -12,34 +12,42 @@ export const MapStats = () => {
   }, [])
 
   return stats ? (
-    <div className="bg-white p-4 m-3 rounded-lg shadow-2xl flex flex-col space-y-3 md:space-y-5 md:w-48">
+    <div className="bg-white p-4 m-3 rounded-lg shadow-2xl flex flex-col space-y-3 md:space-y-5 md:w-52">
       <div className="text-xs md:text-sm font-light italic text-gray-700">
         {`as of ${format(stats.maxDate, 'MMM d, Y')}`}
       </div>
       <div className="flex flex-col items-end space-y-1 md:space-y-3">
         <div className="flex flex-col items-end">
-          <span className="text-lg md:text-3xl font-bold text-teal-600">
+          <span className="text-lg md:text-3xl font-bold text-blue-500">
             {new Intl.NumberFormat().format(stats.newCase.active)}
           </span>
-          <span className="text-xs md:text-sm font-base text-gray-600">new</span>
+          <span className="text-xs md:text-sm font-base text-blue-800">new</span>
         </div>
         <div className="flex flex-col items-end">
-          <span className="text-lg md:text-3xl font-bold text-blue-600">
+          <span className="text-lg md:text-3xl font-bold text-yellow-500">
             {new Intl.NumberFormat().format(stats.totCase.active)}
           </span>
-          <span className="text-xs md:text-sm font-base text-gray-600">active</span>
+          <span className="text-xs md:text-sm font-base text-yellow-800">active</span>
         </div>
         <div className="flex flex-col items-end">
           <span className="text-lg md:text-3xl font-bold text-green-500">
             {new Intl.NumberFormat().format(stats.totCase.recovered)}
           </span>
-          <span className="text-xs md:text-sm font-base text-gray-600">recovered</span>
+          <span className="text-xs md:text-sm font-base text-green-800">recovered</span>
+        </div>
+        <div className="flex flex-col items-end">
+          <span className="text-lg md:text-3xl font-bold text-red-500">
+            {new Intl.NumberFormat().format(stats.totCase.died)}
+          </span>
+          <span className="text-xs md:text-sm font-base text-red-800">deaths</span>
         </div>
         <div className="flex flex-col items-end">
           <span className="text-lg md:text-3xl font-bold text-gray-600">
-            {new Intl.NumberFormat().format(stats.totCase.died)}
+            {new Intl.NumberFormat().format(
+              stats.totCase.active + stats.totCase.recovered + stats.totCase.died
+            )}
           </span>
-          <span className="text-xs md:text-sm font-base text-gray-600">deaths</span>
+          <span className="text-xs md:text-sm font-base text-gray-600">total</span>
         </div>
       </div>
     </div>
