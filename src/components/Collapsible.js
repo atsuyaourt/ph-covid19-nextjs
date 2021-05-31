cfc9ad1e0dc1b36a19206aeb6c5a8c2d48830ec8
Emilio Gozo <emiliogozo@pm.me>
2021-05-31 21:24:41 +0800

    fix: collapse component on load for smaller screens

--- src/components/Collapsible.js
+++ src/components/Collapsible.js
@@ -1,25 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { useWindowSize } from "../hooks";
 
-export const Collapsible = ({ className, children, icon: Icon, btnPosition }) => {
-  const [showContent, setShowContent] = useState(true)
-  const [showButton, setShowButton] = useState(false)
+export const Collapsible = ({
+  className,
+  children,
+  icon: Icon,
+  btnPosition
+}) => {
+  const [showContent, setShowContent] = useState(true);
+  const [showButton, setShowButton] = useState(false);
+  const windowSize = useWindowSize();
 
   useEffect(() => {
-    window.addEventListener('resize', (e) => {
-      const viewCond = e.target.innerWidth < 640
-      setShowButton(viewCond)
-      setShowContent(!viewCond)
-    })
-  }, [])
+    const viewCond = windowSize.width < 640;
+    setShowButton(viewCond);
+    setShowContent(!viewCond);
+  }, [windowSize.width]);
 
   const handleClick = () => {
-    setShowContent(!showContent)
-  }
+    setShowContent(!showContent);
+  };
 
   return (
     <div className={className}>
-      <div className={`${showContent ? 'visible' : 'invisible h-0 w-0'}`}>{children}</div>
+      <div className={`${showContent ? "visible" : "invisible h-0 w-0"}`}>
+        {children}
+      </div>
       {showButton && (
         <button
           className={`absolute top-0 ${btnPosition}-0 m-3 bg-white text-black p-0.5 rounded-sm shadow`}
@@ -29,16 +36,16 @@ export const Collapsible = ({ className, children, icon: Icon, btnPosition }) =>
         </button>
       )}
     </div>
-  )
-}
+  );
+};
 
 Collapsible.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
   icon: PropTypes.elementType,
-  btnPosition: PropTypes.oneOf(['left', 'right']),
-}
+  btnPosition: PropTypes.oneOf(["left", "right"])
+};
 
 Collapsible.defaultProps = {
-  btnPosition: 'left',
-}
+  btnPosition: "left"
+};
--- src/components/Mapbox.js
+++ src/components/Mapbox.js
@@ -1,68 +1,77 @@
-import React, { useState, useRef, useEffect } from 'react'
-import mapboxgl from 'mapbox-gl'
+import React, { useState, useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
 
-import { useRealmApp } from '../RealmApp'
+import { useRealmApp } from "../RealmApp";
 
-import { HiAdjustments, HiAnnotation } from 'react-icons/hi'
+import { HiAdjustments, HiAnnotation } from "react-icons/hi";
 
-import { MapControl, LoadingMsg, MapLegend, MapStats, Collapsible } from '.'
+import { MapControl, LoadingMsg, MapLegend, MapStats, Collapsible } from ".";
 
-mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
-const REDS = ['#ffffff', '#fef0d9', '#fdcc8a', '#fc8d59', '#e34a33', '#b30000']
+const REDS = ["#ffffff", "#fef0d9", "#fdcc8a", "#fc8d59", "#e34a33", "#b30000"];
 
 export const Mapbox = () => {
-  const app = useRealmApp()
-  const mapContainerRef = useRef(null)
-  const [map, setMap] = useState()
-  const [selectedValues, setSelectedValues] = useState({ healthStatus: 'active' })
-  const [legend, setLegend] = useState({})
-  const [showLoadingMsg, setShowLoadingMsg] = useState(true)
-  const [hoveredFeature, _setHoveredFeature] = useState(null)
-  const hoveredFeatureRef = useRef(hoveredFeature)
+  const app = useRealmApp();
+  const mapContainerRef = useRef(null);
+  const [map, setMap] = useState();
+  const [selectedValues, setSelectedValues] = useState({
+    healthStatus: "active"
+  });
+  const [legend, setLegend] = useState({});
+  const [showLoadingMsg, setShowLoadingMsg] = useState(true);
+  const [hoveredFeature, _setHoveredFeature] = useState(null);
+  const hoveredFeatureRef = useRef(hoveredFeature);
 
   const setHoveredFeature = (data) => {
-    hoveredFeatureRef.current = data
-    _setHoveredFeature(data)
-  }
+    hoveredFeatureRef.current = data;
+    _setHoveredFeature(data);
+  };
 
   // initialize map when component mounts
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       // See style options here: https://docs.mapbox.com/api/maps/#styles
-      style: 'mapbox://styles/mapbox/streets-v11',
+      style: "mapbox://styles/mapbox/streets-v11",
       center: [121.774, 12.8797],
-      zoom: 5,
-    })
+      zoom: 5
+    });
 
     // add navigation control (the +/- zoom buttons)
-    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right')
+    map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
 
-    map.once('load', async () => {
-      setMap(map)
+    map.once("load", async () => {
+      setMap(map);
 
-      const caseCountLayer = await app.fetchStatsProv(selectedValues.healthStatus)
+      const caseCountLayer = await app.fetchStatsProv(
+        selectedValues.healthStatus
+      );
 
-      map.addSource('ph-covid19', {
-        type: 'geojson',
-        data: caseCountLayer,
-      })
+      map.addSource("ph-covid19", {
+        type: "geojson",
+        data: caseCountLayer
+      });
 
       map.addLayer({
-        id: 'ph-covid19',
-        type: 'fill',
-        source: 'ph-covid19',
+        id: "ph-covid19",
+        type: "fill",
+        source: "ph-covid19",
         layout: {},
         paint: {
-          'fill-color': '#ffffff',
-          'fill-outline-color': '#000000',
-          'fill-opacity': ['case', ['boolean', ['feature-state', 'hover'], false], 1.0, 0.7],
-        },
-      })
+          "fill-color": "#ffffff",
+          "fill-outline-color": "#000000",
+          "fill-opacity": [
+            "case",
+            ["boolean", ["feature-state", "hover"], false],
+            1.0,
+            0.7
+          ]
+        }
+      });
 
-      map.on('click', 'ph-covid19', (e) => {
-        const { adm2: province, count } = e.features[0].properties
+      map.on("click", "ph-covid19", (e) => {
+        const { adm2: province, count } = e.features[0].properties;
         new mapboxgl.Popup()
           .setLngLat(e.lngLat)
           .setHTML(
@@ -71,130 +80,139 @@ export const Mapbox = () => {
               <div class="text-base font-semibold border-b border-gray-300">${province}</div>
               <div class="flex flex-row">
                 <div class="text-sm font-medium pr-1">Count:</div>
-                <div class="text-sm">${new Intl.NumberFormat().format(count)}</div>
+                <div class="text-sm">${new Intl.NumberFormat().format(
+                  count
+                )}</div>
               </div>
             </div>
             `
           )
-          .addTo(map)
-      })
+          .addTo(map);
+      });
 
-      map.on('mousemove', 'ph-covid19', (e) => {
+      map.on("mousemove", "ph-covid19", (e) => {
         if (e.features.length > 0) {
           if (hoveredFeatureRef.current && hoveredFeatureRef.current > -1) {
             map.setFeatureState(
-              { source: 'ph-covid19', id: hoveredFeatureRef.current },
+              { source: "ph-covid19", id: hoveredFeatureRef.current },
               { hover: false }
-            )
+            );
           }
 
-          let _hoveredFeature = e.features[0].id
+          let _hoveredFeature = e.features[0].id;
 
-          map.setFeatureState({ source: 'ph-covid19', id: _hoveredFeature }, { hover: true })
+          map.setFeatureState(
+            { source: "ph-covid19", id: _hoveredFeature },
+            { hover: true }
+          );
 
-          setHoveredFeature(_hoveredFeature)
+          setHoveredFeature(_hoveredFeature);
         }
-      })
+      });
 
-      map.on('mouseenter', 'ph-covid19', () => {
-        map.getCanvas().style.cursor = 'pointer'
-      })
+      map.on("mouseenter", "ph-covid19", () => {
+        map.getCanvas().style.cursor = "pointer";
+      });
 
-      map.on('mouseleave', 'ph-covid19', () => {
-        map.getCanvas().style.cursor = ''
+      map.on("mouseleave", "ph-covid19", () => {
+        map.getCanvas().style.cursor = "";
         if (hoveredFeatureRef.current) {
           map.setFeatureState(
-            { source: 'ph-covid19', id: hoveredFeatureRef.current },
+            { source: "ph-covid19", id: hoveredFeatureRef.current },
             { hover: false }
-          )
+          );
         }
-        setHoveredFeature(null)
-      })
+        setHoveredFeature(null);
+      });
 
-      map.on('sourcedataloading', (e) => {
-        if (e.sourceId === 'ph-covid19') {
+      map.on("sourcedataloading", (e) => {
+        if (e.sourceId === "ph-covid19") {
           const caseCountArr = e.source.data.features
             .map((o) => o.properties.count)
-            .filter((v) => Number.isInteger(v))
-          setLayerFillColor(e.target, caseCountArr, REDS)
+            .filter((v) => Number.isInteger(v));
+          setLayerFillColor(e.target, caseCountArr, REDS);
         }
-      })
+      });
 
-      map.on('sourcedata', (e) => {
-        if (e.sourceId === 'ph-covid19') {
-          setShowLoadingMsg(false)
+      map.on("sourcedata", (e) => {
+        if (e.sourceId === "ph-covid19") {
+          setShowLoadingMsg(false);
         }
-      })
-    })
+      });
+    });
 
     // clean up on unmount
-    return () => map.remove()
-  }, [])
+    return () => map.remove();
+  }, []);
 
   const updateLayers = async (healthStatus) => {
-    setShowLoadingMsg(true)
-    setSelectedValues({ healthStatus })
+    setShowLoadingMsg(true);
+    setSelectedValues({ healthStatus });
 
     try {
-      const lyrSrc = map.getSource('ph-covid19')
-      lyrSrc.setData(await app.fetchStatsProv(healthStatus, lyrSrc._data))
+      const lyrSrc = map.getSource("ph-covid19");
+      lyrSrc.setData(await app.fetchStatsProv(healthStatus, lyrSrc._data));
     } catch (error) {
-      setShowLoadingMsg(false)
-      console.log(error)
+      setShowLoadingMsg(false);
+      console.log(error);
     }
-  }
+  };
 
   const setLayerFillColor = (map, valArr, colArr) => {
     if (valArr.length > 0) {
-      const _legendLabelArr = []
-      const _legendColArr = []
-      const lyrFillClr = ['interpolate', ['linear'], ['get', 'count']]
+      const _legendLabelArr = [];
+      const _legendColArr = [];
+      const lyrFillClr = ["interpolate", ["linear"], ["get", "count"]];
 
-      const sortedArr = valArr.sort((a, b) => a - b)
-      const minVal = 0
+      const sortedArr = valArr.sort((a, b) => a - b);
+      const minVal = 0;
       const maxVal = valArr.reduce((a, b) => {
-        return a > b ? a : b
-      }, 0)
+        return a > b ? a : b;
+      }, 0);
 
       if (maxVal === minVal) {
-        lyrFillClr.push(minVal)
-        lyrFillClr.push(['to-color', colArr[1]])
+        lyrFillClr.push(minVal);
+        lyrFillClr.push(["to-color", colArr[1]]);
       } else {
-        let plevVal = ''
-        let clevVal = ''
+        let plevVal = "";
+        let clevVal = "";
         colArr.forEach((c, idx) => {
-          const pos = (sortedArr.length - 1) * (idx / colArr.length)
-          const base = Math.floor(pos)
-          const rest = pos - base
+          const pos = (sortedArr.length - 1) * (idx / colArr.length);
+          const base = Math.floor(pos);
+          const rest = pos - base;
           if (sortedArr[base + 1] !== undefined) {
-            clevVal = sortedArr[base] + rest * (sortedArr[base + 1] - sortedArr[base])
+            clevVal =
+              sortedArr[base] + rest * (sortedArr[base + 1] - sortedArr[base]);
           } else {
-            clevVal = sortedArr[base]
+            clevVal = sortedArr[base];
           }
           if (clevVal > 10 && clevVal < 100) {
-            clevVal = Math.ceil(clevVal / 10) * 10
+            clevVal = Math.ceil(clevVal / 10) * 10;
           } else if (clevVal > 100) {
-            clevVal = Math.ceil(clevVal / 100) * 100
+            clevVal = Math.ceil(clevVal / 100) * 100;
           }
           if (clevVal !== plevVal) {
-            _legendLabelArr.push(clevVal)
-            _legendColArr.push(c)
-            lyrFillClr.push(clevVal)
-            lyrFillClr.push(['to-color', c])
-            plevVal = clevVal
+            _legendLabelArr.push(clevVal);
+            _legendColArr.push(c);
+            lyrFillClr.push(clevVal);
+            lyrFillClr.push(["to-color", c]);
+            plevVal = clevVal;
           }
-        })
+        });
       }
 
-      map.setPaintProperty('ph-covid19', 'fill-color', lyrFillClr)
+      map.setPaintProperty("ph-covid19", "fill-color", lyrFillClr);
 
-      setLegend({ label: _legendLabelArr.map((l) => String(l)), color: _legendColArr })
+      setLegend({
+        label: _legendLabelArr.map((l) => String(l)),
+        color: _legendColArr
+      });
     }
-  }
+  };
 
   return (
-    <>
-      <div className="absolute top-0 bottom-0 left-0 right-0" ref={mapContainerRef} />
+    <div className="h-screen w-screen">
+      <div className="absolute inset-0" ref={mapContainerRef} />
       <Collapsible
         className="absolute top-0 left-0 flex flex-col justify-between"
         icon={HiAdjustments}
@@ -211,10 +229,12 @@ export const Mapbox = () => {
         btnPosition="right"
       >
         <div className="flex flex-col">
-          {!showLoadingMsg && <MapLegend title="Number of Cases" legend={legend} />}
+          {!showLoadingMsg && (
+            <MapLegend title="Number of Cases" legend={legend} />
+          )}
         </div>
       </Collapsible>
       {showLoadingMsg && <LoadingMsg />}
-    </>
-  )
-}
+    </div>
+  );
+};
